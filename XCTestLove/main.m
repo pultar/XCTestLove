0d0b68d612b9bd6cf6d9c125b7a43163b50820d6
Felix Pultar <felix.pultar@phys.chem.ethz.ch>
2024-02-10 23:26:14 +0100

    Initial Commit

---
+++ XCTestLove/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XCTestLove
+//
+//  Created by Felix Pultar on 10.02.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
